c9f499b9bd19f53d6aeb4c5997ad8415074b38b7
OriginalEveres <matej.husak@icloud.com>
2025-12-25 23:57:04 +0100

    feat: added fetchCurrentPrice

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,19 @@
  */
 export { PriceProvider } from './price/PriceProvider.js';
 
+/**
+ * Factory function to create price provider instances.
+ */
 export { createPriceProvider } from './price/createPriceProvider.js';
+
+/**
+ * Common types used across the library.
+ */
 export { FIAT } from './types.js';
 export { PriceProviders } from './price/PriceProviders.js';
+
+/**
+ * Function to fetch the current price using the default provider.
+ * FP approach.
+ */
+export { fetchCurrentPrice } from './price/fetchCurrentPrice.js';
